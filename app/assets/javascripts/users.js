5083689b880da0647e01af6deb2cf9e376c0f9a6
Trevor John, tjohn <pair+trevor@john.tj>
2013-03-10 18:54:00 -0400

    Validate email regex with js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,29 @@
+function validateEmail () {
+  function validateEmail() {
+      var regex = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      var $this = $(this);
+      if (regex.test($this.val())) {
+        $this.addClass("success");
+        $this.removeClass("warning");
+        $valid.show();
+        $warning.hide();
+      } else {
+        $this.addClass("warning");
+        $this.removeClass("success");
+        $valid.hide();
+        $warning.show();
+      }
+  }
+  var $email = $("#user_email");
+  var $valid = $email.siblings('.success');
+  var $warning = $email.siblings('.warning');
+  $email.keyup(validateEmail)
+  $email.change(validateEmail);
+  $email.keyup();
+}
+$(document).on('ready', validateEmail);
+$(document).on('page:load', validateEmail);
+
 function passwordMatch () {
   var match = function () {
     if($match.val() == $original.val() && $match.val().length >= 6) {
@@ -22,14 +48,10 @@ function passwordMatch () {
   var $match = $($submit.data("match"));
   var $success = $match.siblings(".success");
   var $error = $match.siblings(".error");
-  $success.hide();
-
-  $submit.addClass('disabled')
-  $submit.prop('disabled', true);
-  $match.addClass("error");
 
   $match.keyup( match );
   $original.keyup( match );
+  $original.keyup();
 };
 
 $(document).on('ready', passwordMatch);
