1e95d9aaa4f81c7ba9d3bee7e5ed97e3a2e2ebd6
Trevor John <trevor@john.tj>
2013-03-09 20:02:18 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bracket::Application.config.secret_key_base = '725b280aeb44fc324b76fc4205f82a7f34ba7e953c85e39bea0e130143804077dfe91d055f98978c690c1713cadf5de94ece68f15f6f2d54df6b694da6d63ff7'
