bf74203e70524a5d0e516bdf887b4ceb9035b626
Trevor John, tjohn <pair+trevor@john.tj>
2013-03-10 17:16:25 -0400

    Add new styling with style guide

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,23 +1,36 @@
-$(function () {
+function passwordMatch () {
+  var match = function () {
+    if($match.val() == $original.val() && $match.val().length >= 6) {
+      $submit.prop('disabled',false);
+      $match.addClass("success");
+      $match.removeClass("error")
+      $success.show();
+      $error.hide();
+      $submit.removeClass('disabled')
+    } else {
+      $match.addClass("error");
+      $match.removeClass("success");
+      $success.hide();
+      $error.show();
+      $submit.prop('disabled',true);
+      $submit.addClass('disabled')
+    }
+  };
 
   var $submit = $("#matchFields");
   var $original = $($submit.data("original"));
   var $match = $($submit.data("match"));
+  var $success = $match.siblings(".success");
+  var $error = $match.siblings(".error");
+  $success.hide();
 
-  $submit.prop('disabled',true);
-  $match.addClass("no_match");
+  $submit.addClass('disabled')
+  $submit.prop('disabled', true);
+  $match.addClass("error");
 
-  $match.keyup(function (event) {
-    if($match.val() == $original.val() && $match.val().length >= 6) {
-      $submit.prop('disabled',false);
-      $match.addClass("match");
-      $match.removeClass("no_match")
-    } else {
-      $match.addClass("no_match");
-      $match.removeClass("match");
-      $submit.prop('disabled',true);
-    }
-  });
+  $match.keyup( match );
+  $original.keyup( match );
+};
 
-});
-
+$(document).on('ready', passwordMatch);
+$(document).on('page:load', passwordMatch);
